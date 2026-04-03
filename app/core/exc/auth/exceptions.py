37cc839b58a97c790e811f69c2e17b12ca167146
Vladyslav Tsurkan <vtsurkan2002@gmail.com>
2026-04-03 23:42:42 +0300

    refactor(exceptions): standardize message exception strings to lowercase

--- app/core/exc/auth/exceptions.py
+++ app/core/exc/auth/exceptions.py
@@ -30,34 +30,34 @@ class VerificationCodeAlreadySentException(ObjectAlreadyExistsException):
 class VerificationFailedOrExpiredException(ObjectNotFoundException):
     def __init__(self, email: str) -> None:
         super().__init__(id_=email, model_name="VerificationCode")
-        self.message = MessageException.VERIFICATION_FAILED_OR_EXPIRED
+        self.message = MessageException.verification_failed_or_expired
 
 
 class InvalidCredentialsException(NotAuthorizedException):
     def __init__(self) -> None:
-        super().__init__(MessageException.COULD_NOT_VALIDATE_CREDENTIALS)
+        super().__init__(MessageException.could_not_validate_credentials)
 
 
 class TokenRefreshException(ForbiddenException):
     def __init__(self) -> None:
-        super().__init__(MessageException.COULD_NOT_REFRESH_TOKEN)
+        super().__init__(MessageException.could_not_refresh_token)
 
 
 class UserDeactivatedException(ForbiddenException):
     def __init__(self, email: str) -> None:
-        super().__init__(MessageException.USER_DEACTIVATED)
+        super().__init__(MessageException.user_deactivated)
         self.alias = {"email": email}
 
 
 class UserNotFoundException(ObjectNotFoundException):
     def __init__(self, email: str) -> None:
         super().__init__(id_=email, model_name="User")
-        self.message = MessageException.USER_NOT_FOUND
+        self.message = MessageException.user_not_found
 
 
 class PasswordInvalidException(ForbiddenException):
     def __init__(self) -> None:
-        super().__init__(MessageException.PASSWORD_INVALID)
+        super().__init__(MessageException.password_invalid)
 
 
 class ForgotPasswordCodeAlreadySentException(ObjectAlreadyExistsException):
@@ -68,4 +68,4 @@ class ForgotPasswordCodeAlreadySentException(ObjectAlreadyExistsException):
 class ForgotPasswordCodeFailedOrExpiredException(ObjectNotFoundException):
     def __init__(self, email: str) -> None:
         super().__init__(id_=email, model_name="ForgotPasswordCode")
-        self.message = MessageException.FORGOT_PASSWORD_CODE_FAILED_OR_EXPIRED
+        self.message = MessageException.forgot_password_code_failed_or_expired
